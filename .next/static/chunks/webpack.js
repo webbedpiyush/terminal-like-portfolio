6a3308ed7c4a8120b124f0c7c37e3359b03ab816
piyush <piyushtiwarindls220@gmail.com>
2024-01-04 08:15:13 +0530

    fixed dark mode

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7a1538102e0d009b"; }
+/******/ 		__webpack_require__.h = function() { return "961a941c58041aee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.1f6d0ab902a5c091.hot-update.js
+++ .next/static/webpack/webpack.1f6d0ab902a5c091.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7a1538102e0d009b"; }
+/******/ 	__webpack_require__.h = function() { return "961a941c58041aee"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
