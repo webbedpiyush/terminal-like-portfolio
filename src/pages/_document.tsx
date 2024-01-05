5c29feea89feffdf16ed5b9566ebbb578499414e
piyush <piyushtiwarindls220@gmail.com>
2024-01-05 14:34:32 +0530

    fixed tailwind css

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html,Main,NextScript } from "next/document";
+
+const Document = () => {
+
+  return ( 
+  <Html className="dark">
+    <Head/>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+  );
+};
+
+export default Document;
